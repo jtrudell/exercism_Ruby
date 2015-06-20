f7582b010a3c18834845f6b8a3eb9b6e5e128a4a
Jen Trudell <jtrudell@gmail.com>
2015-06-20 14:18:27 -0500

    use divmod

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -6,9 +6,9 @@ class Fixnum
                    90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX',
                    5 => 'V', 4 => 'IV', 1 => 'I' }
     roman_hash.each_pair do |letter_value, letter|
-      repeat_roman_letter = number / letter_value
-      roman_number << letter * repeat_roman_letter
-      number -= letter_value * repeat_roman_letter
+      convert_number = number.divmod(letter_value)
+      roman_number << letter * convert_number[0] #quotient of number/letter value
+      number = convert_number[1] #remainder of number/letter_value
     end
     roman_number
   end
