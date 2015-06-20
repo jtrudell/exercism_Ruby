d36a1ca76fcdc047bb28fb9ae6a97b8ea8742d08
Jen Trudell <jtrudell@gmail.com>
2015-06-20 14:36:34 -0500

    made Prime class

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,13 +1,13 @@
-public
-
-def nth(nth_prime, test_array = (2..100000).to_a, primes_array = [])
-  raise ArgumentError, 'Must be an integer greater than 0' if nth_prime <= 0 || nth_prime.integer? == false
-  if primes_array.length != nth_prime
-    p = test_array[0]
-    test_array -= test_array.find_all {|i| i % p == 0}
-    primes_array << p
-    nth(nth_prime, test_array, primes_array)
-  else
-    primes_array[-1]
+class Prime
+  def self.nth(nth_prime, test_array = (2..1_000_000).to_a, primes_array = [])
+    fail ArgumentError, 'Must be an integer greater than 0' if nth_prime <= 0 || nth_prime.integer? == false
+    if primes_array.length != nth_prime
+      p = test_array[0]
+      test_array -= test_array.find_all { |i| i % p == 0 }
+      primes_array << p
+      nth(nth_prime, test_array, primes_array)
+    else
+      primes_array[-1]
+    end
   end
 end
