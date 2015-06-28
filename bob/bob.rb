7aa83461f0925c36c82faea0f277a1b81963828b
Jen Trudell <jtrudell@gmail.com>
2015-06-27 21:08:09 -0500

    revised based on comments

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,11 +1,11 @@
 class Bob
   def hey(remark)
-    remark = remark.strip
-    if remark[-1] == '?' && (!shouting(remark) || include_numbers(remark))
+    remark.strip!
+    if remark[-1] == '?' && (!shouting(remark) || starts_with_numbers(remark))
       'Sure.'
     elsif remark.empty?
       'Fine. Be that way!'
-    elsif shouting(remark) && (!include_numbers(remark) || remark[-1] == '!')
+    elsif shouting(remark) && (!starts_with_numbers(remark) || remark[-1] == '!')
       'Whoa, chill out!'
     else
       'Whatever.'
@@ -16,7 +16,7 @@ class Bob
     remark.upcase == remark
   end
 
-  def include_numbers(remark)
+  def starts_with_numbers(remark)
     remark.to_f > 0
   end
 end
