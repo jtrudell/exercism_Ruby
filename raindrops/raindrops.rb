c6cb88e936d5cd81a479a6199ca48eca65033e13
Jen Trudell <jtrudell@gmail.com>
2015-07-01 21:35:38 -0500

    final

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -3,7 +3,6 @@ class Raindrops
     rain << 'Pling' if number % 3 == 0
     rain << 'Plang' if number % 5 == 0
     rain << 'Plong' if number % 7 == 0
-    rain = number.to_s if rain.empty?
-    rain
+    rain.empty? ? number.to_s : rain
   end
 end
