f5cc335b332ba4858624129633088a1cd4eada6c
Jen Trudell <jtrudell@gmail.com>
2015-07-23 13:32:34 -0500

    passes all tests but ugly

--- binary-search-tree/binary_search_tree.rb
+++ binary-search-tree/binary_search_tree.rb
@@ -1,6 +1,8 @@
 class Bst
   include Enumerable
 
+  ALL_DATA = []
+
   attr_accessor :number, :subs, :left_number, :right_number
 
   def initialize(number)
@@ -15,31 +17,60 @@ class Bst
   end
 
   def left
-    @left_number
+    left_number
   end
 
   def right
-    @right_number
+    right_number
   end
 
   def insert(new_number)
+    if ALL_DATA == []
+      ALL_DATA << number
+    end
     if new_number > number
-      if @right_number == nil
+      if right == nil
         @right_number = Bst.new(new_number)
+        ALL_DATA.insert(ALL_DATA.index(number)+1, new_number)
       else
-        right_number.insert(new_number)
+        right.insert(new_number)
       end
     else
-      if @left_number == nil
+      if left == nil
         @left_number = Bst.new(new_number)
+        ALL_DATA.insert(ALL_DATA.index(number), new_number)
       else
-        left_number.insert(new_number)
+        left.insert(new_number)
       end
     end
   end
 
+  def all_data
+    list = [number]
+  end
+
   def each
-    yield
+    if left != nil then
+      if left_number.left != nil
+      yield left_number.left.data
+      end 
+      yield left_number.data 
+      if left_number.right != nil
+        yield left_number.right.data
+      end
+    end
+    yield number
+    if right != nil then
+      if right_number.left != nil
+      yield right_number.left.data
+      end 
+      yield right_number.data
+      if right_number.right != nil
+        yield right_number.right.data
+      end
+    end
   end
 end
+
+
 
