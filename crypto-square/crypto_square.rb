6b6d166b83a617804efb5035ac73552d7f1a7871
Jen Trudell <jtrudell@gmail.com>
2015-12-01 21:54:59 -0600

    refactored, still don't like method names

--- crypto-square/crypto_square.rb
+++ crypto-square/crypto_square.rb
@@ -1,62 +1,48 @@
 class Crypto
 
   attr_reader :normalize_plaintext, :size
-  attr_accessor :ciphered_string
 
   def initialize(phrase)
     @phrase = phrase
-    @normalize_plaintext = make_plain
+    @normalize_plaintext = convert_to_plain_string
     @size = Math.sqrt(@phrase.length).ceil
-    @ciphered_string = ""
   end
 
-  def make_plain
+  def convert_to_plain_string
     @phrase.downcase!
     @phrase.gsub!(/\W/, "")
   end
 
   def plaintext_segments
-    row_length = size
     segments = []
-    while @phrase.length > 0
-      segments << @phrase.slice!(0, row_length)
+    segments << @phrase.slice!(0, @size) while @phrase.length > 0
+    segments
+  end
+
+  def generate_letters
+    segments = plaintext_segments
+    segments.map! {|row| row.split("")}
+    segments.last << nil until segments.last.length == @size
+    segments
+  end
+
+  def encrypted_segments
+    segments = generate_letters.transpose
+    segments.map! do |segment|
+      segment.delete(nil)
+      segment.join
     end
     segments
   end
 
-  def generate_rows
-    square = plaintext_segments
-    square.map! {|row| row.split("")}
-    until square.last.length == square.length
-      square.last << nil
-    end
-    square
-  end
-
-  def generate_rows_from_columns
-    column_segments = generate_rows.transpose
-    column_segments.map! do |segment|
-      segment.delete(nil)
-      segment.join
-    end
-    column_segments
-  end
-
   def ciphertext
-    ciphered_string = generate_rows_from_columns
-    @ciphered_string = ciphered_string.join
+    encrypted_segments.join
   end
 
   def normalize_ciphertext
-    square = plaintext_segments
-    square.map! {|row| row.split("")}
-    until square.last.length == square.first.length
-      square.last << nil
-    end
-    square.transpose.each_with_index do |row, index|
-      square[index] = row.join
-    end
-    square.join(" ")
+    encrypted_letters = []
+    generate_letters.transpose.each_with_index { |row, index| encrypted_letters[index] = row.join }
+    encrypted_letters.join(" ")
   end
 
 end
