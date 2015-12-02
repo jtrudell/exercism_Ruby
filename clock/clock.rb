8a5866a072fdb3ac85ad5ae94f5be2bc09cf49da
Jen Trudell <jtrudell@gmail.com>
2015-12-02 09:19:34 -0600

    passes first couple of tests

---
+++ clock/clock.rb
@@ -0,0 +1,18 @@
+class Clock
+
+  def self.at(*time)
+    hour = "#{time[0]}"
+    if time[1] != nil
+      minute = "#{time[1]}"
+    else
+      minute = "0"
+    end
+    @time = [hour, minute]
+    @time.map! do |num|
+      if num.to_i < 10
+        num = "0#{num}"
+      end
+    end
+    @time.join(":")
+  end
+end
