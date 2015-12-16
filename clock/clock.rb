4abea656d0aec21057e819ba43225cda17acc9d1
Jen Trudell <jtrudell@gmail.com>
2015-12-15 22:25:45 -0600

    refactor

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,20 +1,21 @@
 class Clock
 
   def self.at(*time)
-    hour = "#{time[0]}"
+    hour_to_minutes = time[0] * 60
     if time[1] != nil
-      minute = "#{time[1]}"
+      minutes = hour_to_minutes + time[1]
     else
-      minute = "0"
+      minutes = hour_to_minutes
     end
-    @time = [hour, minute]
-    @time.map! do |num|
-      if num.to_i < 10
-        num = "0#{num}"
-      else
-        num = num
-      end
-    end
-    @time.join(":")
+    @hour = minutes / 60
+    @minutes = minutes % 60
+    @hour < 10 ? hour = "0#{@hour}" : hour = "#{@hour}"
+    @minutes < 10 ? minutes = "0#{@minutes}" : minutes = "#{@minutes}"
+    "#{hour}:#{minutes}"
+  end
+
+  def +(additional_minutes)
+    @minutes += additional_minutes
+    self.at(@hour, @minutes)
   end
 end
