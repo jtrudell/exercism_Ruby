83818f9796e6378ba76ce50b5e3d2279e523aa71
Jen Trudell <jtrudell@gmail.com>
2016-02-23 21:00:46 -0600

    remove class variable

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,17 +1,16 @@
 class Scrabble
-  @@scoring = {
-    1 => %w(A E I O U L N R S T),
-    2 => %w(D G),
-    3 => %w(B C M P),
-    4 => %w(F H V W Y),
-    5 => %w(K),
-    8 => %w(J X),
-    10 => %w(Q Z)
-  }
-
   def initialize(word)
     @word = word
     @total = 0
+    @scoring = {
+      1 => %w(A E I O U L N R S T),
+      2 => %w(D G),
+      3 => %w(B C M P),
+      4 => %w(F H V W Y),
+      5 => %w(K),
+      8 => %w(J X),
+      10 => %w(Q Z)
+    }
   end
 
   def self.score(word)
@@ -25,6 +24,6 @@ class Scrabble
   end
 
   def score_char(char)
-    @@scoring.each { |point, letter| @total += point if letter.include?(char) }
+    @scoring.each { |point, letter| @total += point if letter.include?(char) }
   end
 end
