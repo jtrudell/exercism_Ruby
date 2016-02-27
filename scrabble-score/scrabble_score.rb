5f65203bc415b5349892f27fca39a3b585a61fa2
Jen Trudell <jtrudell@gmail.com>
2016-02-27 17:03:52 -0600

    refactor

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,16 +1,16 @@
 class Scrabble
+  SCORING = {
+    1 => %w(A E I O U L N R S T),
+    2 => %w(D G),
+    3 => %w(B C M P),
+    4 => %w(F H V W Y),
+    5 => %w(K),
+    8 => %w(J X),
+    10 => %w(Q Z)
+  }
+
   def initialize(word)
-    @word = word
-    @total = 0
-    @scoring = {
-      1 => %w(A E I O U L N R S T),
-      2 => %w(D G),
-      3 => %w(B C M P),
-      4 => %w(F H V W Y),
-      5 => %w(K),
-      8 => %w(J X),
-      10 => %w(Q Z)
-    }
+    @word = word ? word.upcase.strip : ''
   end
 
   def self.score(word)
@@ -18,12 +18,10 @@ class Scrabble
   end
 
   def score
-    return @total if @word.nil? || @word.empty?
-    @word.each_char { |char| score_char(char.upcase) }
-    @total
+    @word.each_char.inject(0) { |sum, char| sum + score_char(char) }
   end
 
   def score_char(char)
-    @scoring.each { |point, letter| @total += point if letter.include?(char) }
+    SCORING.each_pair { |key, value| return key if value.include?(char) }
   end
 end
