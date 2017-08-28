86e5c1bff48a47069623bd7fbeeebab2e9cd9b9c
Jen <jtrudell@gmail.com>
2017-08-28 15:08:15 -0500

    refactor pangram

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -2,15 +2,8 @@ class Pangram
   def self.pangram?(phrase)
     return false if phrase.length < 26
 
-    normalized_phrase = phrase.downcase.gsub(/[^a-z]/, '')
-    char_count = {}
-
-    normalized_phrase.each_char do |c|
-      char_count[c] = true
-      return true if char_count.length == 26
-    end
-
-    false
+    phrase = phrase.downcase.codepoints
+    (97..122).to_a.all? { |cp| phrase.include?(cp) }
   end
 end
 
